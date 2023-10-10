60d33e86503b5b07cddef101ffbd627049f6ec3f
Md Ashrafuzzaman <113747639+imashnobel@users.noreply.github.com>
2023-10-10 13:07:51 +0600

    Create cicd_script.sh

---
+++ scripts/cicd_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+echo "Deploying application ...."
+
+pushd /var/web/html_new/hospital-laravel
+#sudo rm -rf composer.lock
+
+
+git checkout .
+git pull origin dev
+
+sudo /usr/local/bin/composer install --no-dev --optimize-autoloader
+#sudo /usr/local/bin/composer require predis/predis
+#npm install
+
+chmod -R 777 storage
+chmod -R 777 bootstrap/cache
+php artisan config:cache      
+php artisan cache:clear
+php artisan config:clear
+php artisan queue:restart
+
+popd
+sudo chown -Rf nginx:ecom /var/web/html_new/hospital-laravel
+sudo chmod -Rf 770 /var/web/html_new/hospital-laravel
+sudo chmod -Rf g+s /var/web/html_new/hospital-laravel
+sudo systemctl reload php-fpm.service
+sudo nginx -s reload
+
+echo "Application deployed!"

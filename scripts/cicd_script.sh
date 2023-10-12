a80cf2d22b08e755e9bc999ac3379117fc12f120
ashrafnewroz <ashrafuzzaman@newroztech.com>
2023-10-12 12:16:30 +0600

    edit repo

--- scripts/cicd_script.sh
+++ scripts/cicd_script.sh
@@ -14,17 +14,17 @@ sudo /usr/local/bin/composer install --no-dev --optimize-autoloader
 #sudo /usr/local/bin/composer require predis/predis
 #npm install
 
-chmod -R 777 storage
-chmod -R 777 bootstrap/cache
+sudo chmod -R 777 storage
+sudo chmod -R 777 bootstrap/cache
 php artisan config:cache      
 php artisan cache:clear
 php artisan config:clear
 php artisan queue:restart
 
 popd
-sudo chown -Rf nginx:ecom /var/web/html_new/hospital-laravel
-sudo chmod -Rf 770 /var/web/html_new/hospital-laravel
-sudo chmod -Rf g+s /var/web/html_new/hospital-laravel
+sudo chown -Rf nginx:ecom /var/web/html_new/cicd/hospital-laravel/
+sudo chmod -Rf 770 /var/web/html_new/cicd/hospital-laravel/
+sudo chmod -Rf g+s /var/web/html_new/cicd/hospital-laravel/
 sudo systemctl reload php-fpm.service
 sudo nginx -s reload
 
